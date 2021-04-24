6bf08f85d7e94dd92dbff12b73316359fc077c23
Booklordofthedings <Jannis.v.hagen@gmail.com>
2021-04-24 19:37:50 +0200

    changing status

---
+++ startup.bat
@@ -0,0 +1,18 @@
+@echo off 
+    setlocal enableextensions disabledelayedexpansion
+
+    set "search=current status: Offline"
+    set "replace=current status: Online"
+
+    set "textFile=README.md"
+
+    for /f "delims=" %%i in ('type "%textFile%" ^& break ^> "%textFile%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile%" echo(!line:%search%=%replace%!
+        endlocal
+    )
+	
+	git add .
+	git commit -m "changing status"
+	git push
